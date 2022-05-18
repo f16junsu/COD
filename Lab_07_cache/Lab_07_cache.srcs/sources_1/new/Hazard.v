51e7299f3c4fbb294d3b8ec5099dfe86acf23cbe
f16junsu <f16junsu@snu.ac.kr>
2022-05-18 22:09:53 +0900

    start at baseline

---
+++ Lab_07_cache/Lab_07_cache.srcs/sources_1/new/Hazard.v
@@ -0,0 +1,200 @@
+`include "opcodes.v"
+`include "constants.v"
+
+module Hazard(
+    input [1:0] rs_ID,
+    input [1:0] rt_ID,
+    input [1:0] dest_EX,
+    input [1:0] dest_MEM,
+    input use_rs,
+    input use_rt,
+    input RegWrite_EX,
+    input RegWrite_MEM,
+    input valid_IF_ID,
+    input valid_ID_EX,
+
+    input branch_mispredicted,
+    input i_mem_hazard,
+    input d_mem_hazard,
+
+    output reg BTB_forward_PC,
+    output reg stall_PC,
+    output reg stall_IF_ID,
+    output reg stall_ID_EX,
+    output reg stall_EX_MEM,
+    output reg flush_IF_ID,
+    output reg flush_ID_EX,
+    output reg flush_EX_MEM,
+    output reg flush_MEM_WB
+    );
+    wire [3:0] hazards;
+    wire data_hazard;
+    wire control_hazard;
+
+    assign data_hazard = ((rs_ID == dest_EX) &&
+                           use_rs && RegWrite_EX) |
+                          ((rs_ID == dest_MEM) &&
+                           use_rs && RegWrite_MEM) |
+                          ((rt_ID == dest_EX) &&
+                           use_rt && RegWrite_EX) |
+                          ((rt_ID == dest_MEM) &&
+                           use_rt && RegWrite_MEM);
+    assign control_hazard = branch_mispredicted;
+
+    assign hazards[3] = i_mem_hazard;
+    assign hazards[2] = d_mem_hazard;
+    assign hazards[1] = control_hazard;
+    assign hazards[0] = data_hazard;
+
+    always @(*) begin
+        case (hazards)
+            4'b0000: begin
+                BTB_forward_PC = 0;
+                stall_PC = 0;
+                stall_IF_ID = 0;
+                stall_ID_EX = 0;
+                stall_EX_MEM = 0;
+                flush_IF_ID = 0;
+                flush_ID_EX = 0;
+                flush_EX_MEM = 0;
+                flush_MEM_WB = 0;
+            end
+            4'b0001: begin
+                BTB_forward_PC = 0;
+                stall_PC = 1;
+                stall_IF_ID = 1;
+                stall_ID_EX = 0;
+                stall_EX_MEM = 0;
+                flush_IF_ID = 0;
+                flush_ID_EX = 1;
+                flush_EX_MEM = 0;
+                flush_MEM_WB = 0;
+            end
+            4'b0010: begin
+                BTB_forward_PC = 1;
+                stall_PC = 0;
+                stall_IF_ID = 0;
+                stall_ID_EX = 0;
+                stall_EX_MEM = 0;
+                flush_IF_ID = 1;
+                flush_ID_EX = 1;
+                flush_EX_MEM = 1;
+                flush_MEM_WB = 0;
+            end
+            4'b0011: begin
+                BTB_forward_PC = 1;
+                stall_PC = 0;
+                stall_IF_ID = 0;
+                stall_ID_EX = 0;
+                stall_EX_MEM = 0;
+                flush_IF_ID = 1;
+                flush_ID_EX = 1;
+                flush_EX_MEM = 1;
+                flush_MEM_WB = 0;
+            end
+            4'b0100: begin
+                if (valid_IF_ID && valid_ID_EX) begin
+                    BTB_forward_PC = 0;
+                    stall_PC = 1;
+                    stall_IF_ID = 1;
+                    stall_ID_EX = 1;
+                    stall_EX_MEM = 1;
+                    flush_IF_ID = 0;
+                    flush_ID_EX = 0;
+                    flush_EX_MEM = 0;
+                    flush_MEM_WB = 1;
+                end
+                else if (valid_IF_ID && !valid_ID_EX) begin
+                    BTB_forward_PC = 0;
+                    stall_PC = 0;
+                    stall_IF_ID = 0;
+                    stall_ID_EX = 0;
+                    stall_EX_MEM = 1;
+                    flush_IF_ID = 0;
+                    flush_ID_EX = 0;
+                    flush_EX_MEM = 0;
+                    flush_MEM_WB = 1;
+                end
+                else begin
+                    BTB_forward_PC = 0;
+                    stall_PC = 0;
+                    stall_IF_ID = 0;
+                    stall_ID_EX = 1;
+                    stall_EX_MEM = 1;
+                    flush_IF_ID = 0;
+                    flush_ID_EX = 0;
+                    flush_EX_MEM = 0;
+                    flush_MEM_WB = 1;
+                end
+            end
+            4'b1000: begin
+                BTB_forward_PC = 0;
+                stall_PC = 1;
+                stall_IF_ID = 0;
+                stall_ID_EX = 0;
+                stall_EX_MEM = 0;
+                flush_IF_ID = 1;
+                flush_ID_EX = 0;
+                flush_EX_MEM = 0;
+                flush_MEM_WB = 0;
+            end
+            4'b1001: begin
+                BTB_forward_PC = 0;
+                stall_PC = 1;
+                stall_IF_ID = 1;
+                stall_ID_EX = 0;
+                stall_EX_MEM = 0;
+                flush_IF_ID = 0;
+                flush_ID_EX = 1;
+                flush_EX_MEM = 0;
+                flush_MEM_WB = 0;
+            end
+            4'b1010, 4'b1011: begin
+                BTB_forward_PC = 0;
+                stall_PC = 1;
+                stall_IF_ID = 1;
+                stall_ID_EX = 1;
+                stall_EX_MEM = 1;
+                flush_IF_ID = 0;
+                flush_ID_EX = 0;
+                flush_EX_MEM = 0;
+                flush_MEM_WB = 1;
+            end
+            4'b1100: begin
+                if (valid_ID_EX) begin
+                    BTB_forward_PC = 0;
+                    stall_PC = 1;
+                    stall_IF_ID = 1;
+                    stall_ID_EX = 1;
+                    stall_EX_MEM = 1;
+                    flush_IF_ID = 0;
+                    flush_ID_EX = 0;
+                    flush_EX_MEM = 0;
+                    flush_MEM_WB = 1;
+                end
+                else begin
+                    BTB_forward_PC = 0;
+                    stall_PC = 1;
+                    stall_IF_ID = 0;
+                    stall_ID_EX = 0;
+                    stall_EX_MEM = 1;
+                    flush_IF_ID = 1;
+                    flush_ID_EX = 0;
+                    flush_EX_MEM = 0;
+                    flush_MEM_WB = 1;
+                end
+            end
+            4'b1101: begin
+                BTB_forward_PC = 0;
+                stall_PC = 1;
+                stall_IF_ID = 1;
+                stall_ID_EX = 1;
+                stall_EX_MEM = 1;
+                flush_IF_ID = 0;
+                flush_ID_EX = 0;
+                flush_EX_MEM = 0;
+                flush_MEM_WB = 1;
+            end
+        endcase
+    end
+endmodule
---
+++ Lab_07_cache/Lab_07_cache.srcs/sources_1/new/MEM_WB_REG.v
@@ -0,0 +1,75 @@
+`include "constants.v"
+`include "opcodes.v"
+
+module MEM_WB_REG(
+    input clk,
+    input reset_n,
+    input isFlush,
+
+    // Blue WB Block Register
+    input in_isHLT,
+    input in_valid_inst,
+    input in_MemtoReg,
+    input in_RegWrite,
+    input in_isLink,
+    input in_outputenable,
+    output reg out_isHLT,
+    output reg out_valid_inst,
+    output reg out_MemtoReg,
+    output reg out_RegWrite,
+    output reg out_isLink,
+    output reg out_outputenable,
+
+    input [`WORD_SIZE-1:0] in_Memread_data,
+    input [`WORD_SIZE-1:0] in_ALU_result,
+    input [`WORD_SIZE-1:0] in_PC_plus_1,
+    input [`WORD_SIZE-1:0] in_RF_read_data1,
+    input [1:0] in_RFwrite_destination,
+    output reg [`WORD_SIZE-1:0] out_MemRead_data,
+    output reg [`WORD_SIZE-1:0] out_ALU_result,
+    output reg [`WORD_SIZE-1:0] out_PC_plus_1,
+    output reg [`WORD_SIZE-1:0] out_RF_read_data1,
+    output reg [1:0] out_RFwrite_destination
+    );
+    always @(posedge clk or negedge reset_n) begin
+        if (!reset_n) begin
+            out_isHLT <= 0;
+            out_valid_inst <= 0;
+            out_MemtoReg <= 0;
+            out_RegWrite <= 0;
+            out_isLink <= 0;
+            out_outputenable <= 0;
+            out_MemRead_data <= 0;
+            out_ALU_result <= 0;
+            out_PC_plus_1 <= 0;
+            out_RF_read_data1 <= 0;
+            out_RFwrite_destination <= 0;
+        end
+        else if (isFlush) begin
+            out_isHLT <= 0;
+            out_valid_inst <= 0;
+            out_MemtoReg <= 0;
+            out_RegWrite <= 0;
+            out_isLink <= 0;
+            out_outputenable <= 0;
+            out_MemRead_data <= 0;
+            out_ALU_result <= 0;
+            out_PC_plus_1 <= 0;
+            out_RF_read_data1 <= 0;
+            out_RFwrite_destination <= 0;
+        end
+        else begin
+            out_isHLT <= in_isHLT;
+            out_valid_inst <= in_valid_inst;
+            out_MemtoReg <= in_MemtoReg;
+            out_RegWrite <= in_RegWrite;
+            out_isLink <= in_isLink;
+            out_outputenable <= in_outputenable;
+            out_MemRead_data <= in_Memread_data;
+            out_ALU_result <= in_ALU_result;
+            out_PC_plus_1 <= in_PC_plus_1;
+            out_RF_read_data1 <= in_RF_read_data1;
+            out_RFwrite_destination <= in_RFwrite_destination;
+        end
+    end
+endmodule
